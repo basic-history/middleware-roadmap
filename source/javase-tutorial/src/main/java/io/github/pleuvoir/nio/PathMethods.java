30e53cb648baee55cb91486b2ade8848c5a17dac
pleuvoir <pleuvior@foxmail.com>
2019-04-04 14:04:15 +0800

    pathTest

--- source/javase-tutorial/src/main/java/io/github/pleuvoir/nio/PathMethods.java
+++ source/javase-tutorial/src/main/java/io/github/pleuvoir/nio/PathMethods.java
@@ -1,6 +1,7 @@
 package io.github.pleuvoir.nio;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,7 +13,7 @@ import java.nio.file.Paths;
 public class PathMethods {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException {
 		
 		// Paths.get 是 FileSystems.getDefault().getPath的简写
 		Path path = Paths.get("./xanadu.txt");
@@ -30,5 +31,11 @@ public class PathMethods {
 		
 		URI uri = normalize.toUri();  // 转换为一个浏览器可以打开的地址
 		System.out.println(uri);
+		
+		// 读取classpath
+		//D:\03 space\01 git\middleware-roadmap\source\javase-tutorial\target\classes\xanadu04.txt 新增的文件会在此目录下出现
+		//Path currentDir = Paths.get(ClassLoader.getSystemResource("xanadu04.txt").toURI());
+		Path currentDir = Paths.get(Thread.currentThread().getContextClassLoader().getResource("xanadu5.txt").toURI());
+		System.out.println(currentDir.toAbsolutePath());
 	}
 }
