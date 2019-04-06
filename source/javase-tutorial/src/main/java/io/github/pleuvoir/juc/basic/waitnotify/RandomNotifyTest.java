97fc681cb4c87f7002e98d92c72190da809eb4bb
pleuvoir <pleuvior@foxmail.com>
2019-04-06 23:13:52 +0800

    等待通知

---
+++ source/javase-tutorial/src/main/java/io/github/pleuvoir/juc/basic/waitnotify/RandomNotifyTest.java
@@ -0,0 +1,86 @@
+package io.github.pleuvoir.juc.basic.waitnotify;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 演示线程的通知是从对象的等待队列随机选择
+ *
+ */
+public class RandomNotifyTest {
+
+	static Monitor monitor = new Monitor();
+
+	public static void main(String[] args) {
+
+		Thread waitGetNameThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					monitor.waitGetName();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		Thread waitGetTimestampThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					monitor.waitGetTimestamp();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+
+		waitGetTimestampThread.setName("waitGetTimestampThread");
+		waitGetTimestampThread.start();
+		
+		waitGetNameThread.setName("waitGetNameThread");
+		waitGetNameThread.start();
+
+		//改变名字并通知，会发现它是随机通知 正在等待获取对象锁的线程
+		new Thread(() -> {monitor.changeName("pleuvoir");}).start();
+	}
+
+	private static class Monitor {
+		private String name = "default";
+		private long timestamp = -1L;
+
+		public synchronized String waitGetName() throws InterruptedException {
+			System.out.println(Thread.currentThread().getName() + "获取到对象锁" + System.currentTimeMillis());
+			wait();
+			System.out.println(Thread.currentThread().getName() + " wait返回了：waitGetName()=" + name);
+			return name;
+		}
+
+		public synchronized long waitGetTimestamp() throws InterruptedException {
+			System.out.println(Thread.currentThread().getName() + "获取到对象锁" + System.currentTimeMillis());
+			wait();
+			System.out.println(Thread.currentThread().getName() + " wait返回了：waitGetTimestamp()=" + timestamp);
+			return timestamp;
+		}
+
+		public synchronized void changeName(String name) {
+			this.name = name;
+			notify();
+			//notifyAll();
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		@SuppressWarnings("unused")
+		public void changeTimestamp(long timestamp) {
+			this.timestamp = timestamp;
+			notify();
+		}
+
+	}
+}
--- source/javase-tutorial/src/main/java/io/github/pleuvoir/juc/basic/waitnotify/WaitNotifyTest.java
+++ source/javase-tutorial/src/main/java/io/github/pleuvoir/juc/basic/waitnotify/WaitNotifyTest.java
@@ -1,4 +1,4 @@
-package io.github.pleuvoir.juc.basic;
+package io.github.pleuvoir.juc.basic.waitnotify;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
