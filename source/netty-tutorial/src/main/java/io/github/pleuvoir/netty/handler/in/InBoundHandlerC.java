c320842d168e78cf6c25f65f943bd78802ebe924
pleuvoir <pleuvior@foxmail.com>
2019-04-29 13:57:28 +0800

    pipeline 和handler

---
+++ source/netty-tutorial/src/main/java/io/github/pleuvoir/netty/handler/in/InBoundHandlerC.java
@@ -0,0 +1,18 @@
+package io.github.pleuvoir.netty.handler.in;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class InBoundHandlerC extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("InBoundHandlerC: " + msg);
+
+        
+        ctx.pipeline().remove("out-C");
+     //   ctx.channel().writeAndFlush(msg);  //注意这里，如果不这样 则无法进入下一个 out
+        
+        //找到上一个outbound,然后进行传播 ；这个代码跳过OutBoundHandler是因为OutBoundHandler都在后面。
+        ctx.writeAndFlush(msg);
+    }
+}
---
+++ source/netty-tutorial/src/main/java/io/github/pleuvoir/netty/handler/out/OutBoundHandlerA.java
@@ -0,0 +1,14 @@
+package io.github.pleuvoir.netty.handler.out;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+
+public class OutBoundHandlerA extends ChannelOutboundHandlerAdapter {
+
+	@Override
+	public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+		System.out.println("OutBoundHandlerA: " + msg);
+		super.write(ctx, msg, promise); //传递给下一个
+	}
+}
