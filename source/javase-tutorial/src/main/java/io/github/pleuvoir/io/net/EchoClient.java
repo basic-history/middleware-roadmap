6f1bbdcfe621d373b9dad7eca4c9899ae1f763af
pleuvoir <pleuvior@foxmail.com>
2019-05-01 18:37:26 +0800

    修改

--- source/javase-tutorial/src/main/java/io/github/pleuvoir/io/net/EchoClient.java
+++ source/javase-tutorial/src/main/java/io/github/pleuvoir/io/net/EchoClient.java
@@ -9,7 +9,10 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+
+import com.google.common.util.concurrent.MoreExecutors;
 
 public class EchoClient {
 	
@@ -31,13 +34,17 @@ public class EchoClient {
 		for (int i = 0; i < CONNECT_COUNT; i++) {
 			tp.execute(new ConnectTask());
 		}
-		tp.shutdown();
+		System.out.println("关闭连接池1");
+		//如果没有释放连接 关闭线程池也不起作用 不能终止
+		MoreExecutors.shutdownAndAwaitTermination(tp, 1, TimeUnit.SECONDS);
+		System.out.println("关闭连接池2");
 	}
 
 	public static class ConnectTask implements Runnable {
 		@Override
 		public void run() {
-			try (Socket client = new Socket();) {
+			try(Socket client = new Socket();)  { //自动释放
+				
 				CYCLICBARRIER.await();
 				client.connect(new InetSocketAddress("127.0.0.1", 8000));
 				System.out.println("echoClient connect success.");
@@ -49,7 +56,8 @@ public class EchoClient {
 					// 从一个字符输入流中读取文本
 					BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
 					System.out.println(br.readLine());
-					System.out.println(br.readLine());
+					//System.out.println(br.readLine());
+					System.out.println("end");
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
