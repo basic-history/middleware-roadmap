72587e056bba1d4fc02b146e3f2c902bd09d8747
pleuvoir <pleuvior@foxmail.com>
2019-05-16 16:50:24 +0800

    Table

---
+++ source/javase-tutorial/src/main/java/io/github/pleuvoir/more/TableTest.java
@@ -0,0 +1,49 @@
+package io.github.pleuvoir.more;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.google.common.base.Supplier;
+import com.google.common.collect.Table;
+import com.google.common.collect.Tables;
+
+/**
+ * 类似EXCEL表格一样
+ * @author pleuvoir
+ *
+ */
+public class TableTest {
+
+	private static Table<String, String, Object> hsmServices = Tables
+			.newCustomTable(new ConcurrentHashMap<String, Map<String,Object>>(), new Supplier<Map<String,Object>>(){
+		@Override
+		public Map<String, Object> get() {
+			return new ConcurrentHashMap<String, Object>();
+		}
+	});
+	
+	
+	
+	/**
+	 * 		
+	 * 
+	 *   ------------------------
+	 *   |	行1-列1	|	行1-列2	|
+	 *   |----------------------
+	 *   |	行2-列1	|	行2-列2	|
+	 *   -----------------------|
+	 * 
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+		hsmServices.put("行1", "列1", "行1-列1");
+		hsmServices.put("行1", "列2", "行1-列2");
+		hsmServices.put("行2", "列1", "行2-列1");
+		hsmServices.put("行2", "列2", "行2-列2");
+		
+		Object object = hsmServices.get("行1", "列1");
+		System.out.println(object);
+	}
+}
---
+++ source/javase-tutorial/src/main/java/io/github/pleuvoir/more/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * 
+ * GUAVA工具使用
+ * @author pleuvoir
+ *
+ */
+package io.github.pleuvoir.more;
